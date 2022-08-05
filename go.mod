b59477477df0060549bc5980d76057ecc6e2d6dd
Sam Bossley <bossley.samuel@gmail.com>
2022-08-05 14:36:31 -0700

    implement @solene handler

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module git.sr.ht/~bossley9/feedme
 go 1.18
 
 require github.com/gorilla/mux v1.8.0
+
+require git.sr.ht/~bossley9/gem v1.1.0
--- pkg/handlers/solene.go
+++ pkg/handlers/solene.go
@@ -1,9 +1,80 @@
 package handlers
 
 import (
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"strings"
+	"time"
+
+	"git.sr.ht/~bossley9/feedme/pkg/api"
+	"git.sr.ht/~bossley9/feedme/pkg/atom"
+
+	"git.sr.ht/~bossley9/gem"
 )
 
+type soleneResponse struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel struct {
+		Title string `xml:"title"`
+		Items []struct {
+			Title       string `xml:"title"`
+			Description string `xml:"description"`
+			Guid        string `xml:"guid"`
+			Link        string `xml:"link"`
+			PubDate     string `xml:"pubDate"`
+		} `xml:"item"`
+	} `xml:"channel"`
+}
+
 func HandleSolene(w http.ResponseWriter, r *http.Request) {
-	HandleUnimplemented(w, r)
+	url := "https://dataswamp.org/~solene/rss.xml"
+	raw, err := api.FetchGet(url)
+	if err != nil {
+		HandleBadRequest(w, r, err)
+		return
+	}
+
+	var response soleneResponse
+	if err := xml.Unmarshal(raw, &response); err != nil {
+		HandleBadRequest(w, r, err)
+		return
+	}
+
+	data := response.Channel
+
+	// use the current time as feed update time
+	feed, err := atom.CreateFeed(url, data.Title, time.Now())
+	if err != nil {
+		HandleBadRequest(w, r, err)
+		return
+	}
+
+	feed.AddLink(url, atom.RelSelf)
+
+	for _, item := range data.Items {
+		published, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			published = time.Now()
+		}
+
+		entry, err := atom.CreateFeedEntry(item.Guid, item.Title, published)
+		if err != nil {
+			continue
+		}
+
+		entry.AddLink(item.Link, atom.RelSelf)
+		entry.SetPublished(published)
+
+		spaceTrimmedContent := strings.TrimSpace(item.Description)
+		geminiContent := strings.TrimSuffix(strings.TrimPrefix(spaceTrimmedContent, "<pre>"), "</pre>")
+		content := gem.ToHTML(geminiContent)
+
+		entry.SetContent(content, "html")
+
+		feed.AddEntry(entry)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, feed.String())
 }
